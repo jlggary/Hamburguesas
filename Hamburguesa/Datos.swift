32f98b87f5a71525e6af2daba1e9fd942c8abf60
Jose Garcia <josegarcia@Joses-Mac.local>
2017-01-05 02:00:29 -0800

    con 2 clases

--- Hamburguesa/Datos.swift
+++ Hamburguesa/Datos.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+class ColeccionDePaises{
+    var paises : [String] = ["Perú", "Cuba", "Argentina", "Chile", "Méjico", "Ecuador", "España", "Francia", "Inglaterra", "Italia", "Suiza", "Alemania", "USA", "Noruega", "Suecia", "Rusia", "China", "Japón", "Australia", "Nueva Zelanda"]
+    
+    func obtenPais() -> String {
+        let posicion : Int = Int(arc4random()) % paises.count
+        return paises[posicion]
+    }
+}
+
+class ColeccionDeHamburguesas{
+    var hamburguesas : [String] = ["con tomate", "con queso", "con bacon", "con huevo", "con tomate y queso", "con tomate y bacon", "con tomate y huevo", "con quedo y bacon", "con queso y huevo", "con bacon y huevo", "con tomate, queso y bacon", "con tomate, queso y huevo", "con queso, bacon y huevo", "vegetal", "de pollo", "de pescado", "con pepinillos", "completa", "sin gluten", "sencilla"]
+    
+    func obtenHamburguesa() -> String {
+        let posicion : Int = Int(arc4random()) % hamburguesas.count
+        return hamburguesas[posicion]
+    }
+    
+}
